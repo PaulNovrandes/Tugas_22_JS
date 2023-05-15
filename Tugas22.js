d9b139d227868485cf1b6ab22b4b7b20c8e2faf3
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-05-15 18:51:06 +0700

    Tugas 22 JS
    Membuat forEach. Tugas ini dikumpulkan kepada niomic.

---
+++ Tugas22.js
@@ -0,0 +1,13 @@
+var data = "Saya ingin belajar bersama";
+var data2;
+
+function panggilForEach(){
+  data2 = data.split(' ');
+  // console.log(data2);
+  data2.forEach((item, i) => {
+    console.log("Item : ", item,"index ke", i);
+  });
+
+}
+
+panggilForEach();
