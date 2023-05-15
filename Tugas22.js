159233bc2814c6e00f32db94af6b6b21b41e693f
PaulNovrandes <novrandes.tanuma@gmail.com>
2023-05-15 19:56:08 +0700

    Tugas 22 JS
    menambah console log string.

--- Tugas22.js
+++ Tugas22.js
@@ -2,6 +2,7 @@ var data = "Saya ingin belajar bersama";
 var data2;
 
 function panggilForEach(){
+  console.log(data);
   data2 = data.split(' ');
   // console.log(data2);
   data2.forEach((item, i) => {
